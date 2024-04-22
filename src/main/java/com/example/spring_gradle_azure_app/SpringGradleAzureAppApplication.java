2023d015cd889ff9cc72a7eae83049d7b8c49bec
ABHINAVSENGARCS_1CFD43E94DB3 <abhinavsengar.cs@gmail.com>
2024-04-22 10:44:26 +0530

    gradle github actions

---
+++ src/main/java/com/example/spring_gradle_azure_app/SpringGradleAzureAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_gradle_azure_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGradleAzureAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGradleAzureAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_gradle_azure_app/SpringGradleAzureAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_gradle_azure_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGradleAzureAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
